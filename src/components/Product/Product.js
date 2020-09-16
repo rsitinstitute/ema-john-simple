47f8fbbdc0c08568bff05591e970ab44d5fb22d1
rsitinstitute <rsitinstitute@gmail.com>
2020-09-16 23:55:40 +0600

    update

---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+    console.log(props.product);
+    return (
+        <div>
+            <h3>this is Product</h3>
+        </div>
+    );
+};
+
+export default Product;
